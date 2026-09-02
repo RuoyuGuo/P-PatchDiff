d19161ff47647cbca847fc872aba87eef8a321f0
ruoyu <ruoyu@ruoyu-pc.cse.unsw.EDU.AU>
2026-09-02 17:03:56 +1000

    update arch

--- PPatchDiff/test.sh
+++ PPatchDiff/test.sh
@@ -1,3 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/infer_lolv1_pgc.yaml
-CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/infer_lolv2_pgc.yaml
-CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/infer_lolsyn_pgc.yaml
+# CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/infer_lolv1_pgc.yaml
+# CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/infer_lolv2_pgc.yaml
+# CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/infer_lolsyn_pgc.yaml
+
+
+CUDA_VISIBLE_DEVICES=0 python pydiff/train.py -opt options/tt.yaml
